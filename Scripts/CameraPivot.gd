471441b3144b79ff25e5d5c090ec4fc70ba51997
Suvit Angirash <suvit.angirash2@gmail.com>
2026-01-11 18:54:13 +0400

    character

---
+++ Scripts/CameraPivot.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+@export var target_node: Node3D# target position (to follow the character)
+
+@export var mouse_sensitivity = 0.1# camera rotation speed
+
+@export var physical_skel:Skeleton3D# character physical skeleton
+
+@onready var spring_arm = $SpringArm3D
+
+var mouse_lock = false # is mouse locked
+
+func _ready():
+	mouse_lock = true
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	for child in physical_skel.get_children():
+		# prevent the camera from clipping into the character
+		if child is PhysicalBone3D:spring_arm.add_excluded_object(child.get_rid())
+		
+	if target_node != null:
+		# lerp position to the target position
+		global_position = lerp(global_position,target_node.global_position,0.5)
+
+func _input(event):
+	# mouse lock
+	if Input.is_action_just_pressed("exit_camera"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			mouse_lock = false
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			mouse_lock = true
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	#rotate camera
+	if event is InputEventMouseMotion and mouse_lock:
+		rotation_degrees.y -= mouse_sensitivity*event.relative.x
+		rotation_degrees.x -= mouse_sensitivity*event.relative.y
+		rotation_degrees.x = clamp(rotation_degrees.x,-45,45)
+	
+		
